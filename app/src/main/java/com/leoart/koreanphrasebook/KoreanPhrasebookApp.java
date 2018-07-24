3a6b62195959b630b46209354eaf990c2e286ef9
Maryan Onysko <maryan.onysko@gmail.com>
2018-07-24 22:14:23 +0300

    remove realm

--- app/src/main/java/com/leoart/koreanphrasebook/KoreanPhrasebookApp.java
+++ app/src/main/java/com/leoart/koreanphrasebook/KoreanPhrasebookApp.java
@@ -2,9 +2,6 @@ package com.leoart.koreanphrasebook;
 
 import android.support.multidex.MultiDexApplication;
 
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
-
 /**
  * Created by bogdan on 6/18/17.
  */
@@ -14,10 +11,5 @@ public class KoreanPhrasebookApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Realm.init(this);
-        RealmConfiguration config = new RealmConfiguration.Builder()
-                .deleteRealmIfMigrationNeeded()
-                .build();
-        Realm.setDefaultConfiguration(config);
     }
 }
